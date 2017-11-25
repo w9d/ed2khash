7cdb2e27e2b37db623699480fb7217bffeb93b44
w9d <w9d@localhost>
2017-11-25 23:30:36 +0000

    Added basic md4 use to test. Don't like the performance.
    It does each 9728000 byte block in about 560ms on my meagre laptop.
    That means about 69 seconds for 1.2GB of data. This is single threaded.
    Using webworkers should speed this up to about 45 seconds for 1.2GB.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//import md4 from "../external/js-md4/src/md4";
+const md4 = require("../external/js-md4/src/md4");
+
 function generateZeroData(start, end, size) {
   if (end > size)
     end = size;
@@ -51,7 +54,7 @@ FileReader.prototype.JUSTDOIT = function(name, file, othis) {
   }
 }
 FileReader.prototype.readAsArrayBuffer = function(file) {
-  setTimeout(this.JUSTDOIT, 100, 'loadend', file, this);
+  setTimeout(this.JUSTDOIT, 10, 'loadend', file, this);
 }
 FileReader.prototype.addEventListener = function(event, fn, useCapture) {
   if (this.callback['event_' + event] == undefined)
@@ -60,23 +63,39 @@ FileReader.prototype.addEventListener = function(event, fn, useCapture) {
   this.callback['event_' + event].push(fn);
 }
 
-let file = new File(generateZeroData, 9728000);
+let file = new File(generateZeroData, 190456000);
 let mockfilelist = [];
 mockfilelist.push(file);
 
+var timings = [];
+var total = 0, min = 60000, max = 0;
 
 let reader = new FileReader();
 for (let i = 0; i < file.size; i += 9728000) {
-  console.log('read',i,'to',i+9728000);
+  //console.log('read',i,'to',i+9728000);
   reader.readAsArrayBuffer(file.slice(i, i + 9728000));
-  console.log('finished read');
+  //console.log('finished read');
 }
 reader.addEventListener('loadend',
   function(evt) {
-    console.log('callback', evt.target.result.byteLength)
-    var test = new Uint8Array(evt.target.result);
+    //console.log('callback', evt.target.result.byteLength)
+    //var test = new Uint8Array(evt.target.result);
 
-    console.log(test[5]);
+    var before = Date.now();
+    md4.hex(evt.target.result);
+    var after = Date.now();
+    var diff = after - before;
+    timings.push(diff);
+
+    if (diff < min)
+        min = diff;
+    if (diff > max)
+        max = diff;
+    total += diff;
+
+    //console.log("MD4 calculation time...");
+    console.log('md4 min/avg/max = ' + min + '/' + Math.floor((total/timings.length)+0.5) + '/' + max);
+
   }, false
 );
 
