115b504f5ab376f2b3c160bf703e9bb5bdc87737
w9d <w9d@localhost>
2018-02-09 00:44:21 +0000

    Use self.onmessage instead of onmessage.
    Only did this originally because one of the useless node webworker modules
    needed it.

--- md4-worker.js
+++ md4-worker.js
@@ -1,6 +1,6 @@
 importScripts('external/js-md4/src/md4.js');
 
-onmessage = function(e) {
+self.onmessage = function(e) {
   var result = md4.arrayBuffer(e.data.data);
   postMessage({'workerid': e.data.workerid, 'index': e.data.index,
     'md4': result, 'dirty': e.data.data}, [result, e.data.data]);
