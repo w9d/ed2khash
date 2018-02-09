0595903e6eec9ec2875ad83c5adcdf3a835b821a
w9d <w9d@localhost>
2018-02-09 01:27:10 +0000

    Add some early ed2k tests. Many more will be coming.

--- test/ed2k_test.js
+++ test/ed2k_test.js
@@ -12,27 +12,63 @@ const test = require('tape');
 const com = require('./common.js');
 const md4 = require('../external/js-md4/src/md4.js');
 
-var window = { Worker: null };
 const ed2k = require('../ed2k_hasher.js');
-const webworker = require('webworker-threads').Worker;
-const fakewindow = { Worker: webworker, FileReader: com.MockFileReader };
+const fakewindow = { FileReader: com.MockFileReader, alert: (function(noop){}) };
 const fakenavigator = { hardwareConcurrency: 1 };
 ed2k.passMocks(fakewindow, fakenavigator, md4);
 
 process.chdir('..');
 
-test('single file single chunk with nullend', function(t) {
+test('single file single chunk-1 zeros with nullend', function(t) {
     t.plan(1);
-
-    var test = new com.MockFile(null, 'name', {});
-    test.size = 9728000; test._end = 9728000;
-    test._genFunc = com.genZero;
-    var hash = [];
+    var test = new com.MockFile()._test(com.genZero, 9727999);
     var c = function(_file, _hash) {
-      hash[_file.name] = _hash;
-      console.log(hash);
+      t.equal(_hash, 'ac44b93fc9aff773ab0005c911f8396f');
     };
-    ed2k.ed2k_files([test]);
-    
-    t.equal(true, true);
+    ed2k.ed2k_files([test], true, null, c);
+});
+
+test('single file single chunk zeros with nullend', function(t) {
+    t.plan(1);
+    var test = new com.MockFile()._test(com.genZero, 9728000);
+    var c = function(_file, _hash) {
+      t.equal(_hash, 'fc21d9af828f92a8df64beac3357425d');
+    };
+    ed2k.ed2k_files([test], true, null, c);
+});
+
+test('single file single chunk+1 zeros with nullend', function(t) {
+    t.plan(1);
+    var test = new com.MockFile()._test(com.genZero, 9728001);
+    var c = function(_file, _hash) {
+      t.equal(_hash, '06329e9dba1373512c06386fe29e3c65');
+    };
+    ed2k.ed2k_files([test], true, null, c);
+});
+
+test('single file two chunks-1 zeros with nullend', function(t) {
+    t.plan(1);
+    var test = new com.MockFile()._test(com.genZero, 19455999);
+    var c = function(_file, _hash) {
+      t.equal(_hash, 'a4aed104a077de7e4210e7f5b131fe25');
+    };
+    ed2k.ed2k_files([test], true, null, c);
+});
+
+test('single file two chunks zeros with nullend', function(t) {
+    t.plan(1);
+    var test = new com.MockFile()._test(com.genZero, 19456000);
+    var c = function(_file, _hash) {
+      t.equal(_hash, '114b21c63a74b6ca922291a11177dd5c');
+    };
+    ed2k.ed2k_files([test], true, null, c);
+});
+
+test('single file two chunks+1 zeros with nullend', function(t) {
+    t.plan(1);
+    var test = new com.MockFile()._test(com.genZero, 19456001);
+    var c = function(_file, _hash) {
+      t.equal(_hash, 'e57f824d28f69fe90864e17673668457');
+    };
+    ed2k.ed2k_files([test], true, null, c);
 });
