e3a6d53292e7a07687cc17e92d4f65c9c86cba34
w9d <w9d@localhost>
2018-03-14 22:29:37 +0000

    Create new basic ed2k_files that lives entirely in web worker.
    This is much cleaner and shaves about 200 lines off of the first
    attempt. It'd be nice to be able to test it though.

--- ed2k_hasher_basic.js
+++ ed2k_hasher_basic.js
@@ -1,83 +1,52 @@
-var ed2k_files = (function(files, opts) {
+var ed2k_files = function(files, opts) {
 'use strict';
-  var reader = new window.FileReader();
+  var worker = null;
   var prop = { onprogress: null, onfilecomplete: null, onallcomplete: null,
-    execute: execute };
-  var opts = (opts === undefined && {} || opts);
-
-  function ed2k_file(file, offset, md4_list) {
-    'use strict';
-    if (typeof offset !== 'number') {
-      offset = 0;
-      md4_list = [];
-    }
-
-    reader.onloadend = function(evt) {
-      md4_list.push(md4.arrayBuffer(evt.target.result));
-      ed2k_file(file, offset+9728000, md4_list);
-    }
-
-    if (offset <= file.size) {
-      reader.readAsArrayBuffer(file.slice(offset, offset+9728000));
-    } else {
-      var ed2k_hash = md4.create();
-      if (file.size >= 9728000) {
-        // calculate final hash...
-        for (var i = 0, chunkhash; chunkhash = md4_list[i]; i++) {
-          ed2k_hash.update(chunkhash);
-        }
-
-        (prop.onfilecomplete) && prop.onfilecomplete(file, ed2k_hash.hex());
-      } else {
-        (prop.onfilecomplete) && prop.onfilecomplete(file,
-            arrayBufferToHexDigest(md4_list[0]));
-      }
-    }
-  }
-
-  function arrayBufferToHexDigest(arr) {
-    // taken from js-md4
-    var HEX_CHARS = '0123456789abcdef'.split('');
-    var blocks = new Uint32Array(arr);
-    var h0 = blocks[0], h1 = blocks[1], h2 = blocks[2], h3 = blocks[3];
-
-    return HEX_CHARS[(h0 >> 4) & 0x0F] + HEX_CHARS[h0 & 0x0F] +
-    HEX_CHARS[(h0 >> 12) & 0x0F] + HEX_CHARS[(h0 >> 8) & 0x0F] +
-    HEX_CHARS[(h0 >> 20) & 0x0F] + HEX_CHARS[(h0 >> 16) & 0x0F] +
-    HEX_CHARS[(h0 >> 28) & 0x0F] + HEX_CHARS[(h0 >> 24) & 0x0F] +
-    HEX_CHARS[(h1 >> 4) & 0x0F] + HEX_CHARS[h1 & 0x0F] +
-    HEX_CHARS[(h1 >> 12) & 0x0F] + HEX_CHARS[(h1 >> 8) & 0x0F] +
-    HEX_CHARS[(h1 >> 20) & 0x0F] + HEX_CHARS[(h1 >> 16) & 0x0F] +
-    HEX_CHARS[(h1 >> 28) & 0x0F] + HEX_CHARS[(h1 >> 24) & 0x0F] +
-    HEX_CHARS[(h2 >> 4) & 0x0F] + HEX_CHARS[h2 & 0x0F] +
-    HEX_CHARS[(h2 >> 12) & 0x0F] + HEX_CHARS[(h2 >> 8) & 0x0F] +
-    HEX_CHARS[(h2 >> 20) & 0x0F] + HEX_CHARS[(h2 >> 16) & 0x0F] +
-    HEX_CHARS[(h2 >> 28) & 0x0F] + HEX_CHARS[(h2 >> 24) & 0x0F] +
-    HEX_CHARS[(h3 >> 4) & 0x0F] + HEX_CHARS[h3 & 0x0F] +
-    HEX_CHARS[(h3 >> 12) & 0x0F] + HEX_CHARS[(h3 >> 8) & 0x0F] +
-    HEX_CHARS[(h3 >> 20) & 0x0F] + HEX_CHARS[(h3 >> 16) & 0x0F] +
-    HEX_CHARS[(h3 >> 28) & 0x0F] + HEX_CHARS[(h3 >> 24) & 0x0F];
-  }
+    execute: execute, terminate: terminate };
 
   function execute() {
-    for (var i = 0; i<files.length; i++) {
-      ed2k_file(files[i], undefined, undefined);
+    if (!window.Worker) {
+      window.alert('Browser does not support HTML5 Web Workers.\n\nYou cannot' +
+        ' use this browser. Sorry.');
+      return;
+    }
+
+    worker = new Worker('ed2k_hasher_basic-worker.js');
+    worker.onerror = function(e) {
+      window.alert('Something wrong with HTML5 Web Worker. The error is...\n\n'
+          + e.message);
+    }
+    worker.onmessage = function(e) {
+      if (e.data.event === 1) {
+        // progress event
+        (prop.onprogress) && prop.onprogress(e.data.file, e.data.file_progress,
+            e.data.files_progress);
+        //console.log('chunk processed ' + Date.now());
+      } else if (e.data.event === 2) {
+        // onfilecomplete event
+        (prop.onfilecomplete) && prop.onfilecomplete(e.data.file, e.data.ed2k_hash);
+      } else if (e.data.event === 3) {
+        // onallcomplete event
+        (prop.onallcomplete) && prop.onallcomplete();
+      } else if (e.data.event === 100) {
+        // md4 sub worker error
+        worker.terminate();
+        window.alert('Something wrong with HTML5 Sub Web Worker. The error is...\n\n'
+            + e.data.message);
+        worker = null;
+      } else {
+        window.alert('This should never ever happen.');
+      }
+    }
+    worker.postMessage({files: files});
+  }
+
+  function terminate() {
+    if (worker) {
+      worker.terminate();
+      worker = null;
     }
   }
 
   return prop;
-});
-
-if (typeof window !== 'object' && typeof process === 'object' &&
-    process.versions && process.versions.node) {
-  var passMocks = function(_window, _navigator, _md4) {
-    window = _window;
-    navigator = _navigator;
-    md4 = _md4;
-  };
-  ED2K_NODE_ENVIRON = true;
-  module.exports = {
-    ed2k_files: ed2k_files,
-    passMocks: passMocks
-  };
 }
