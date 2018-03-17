e5944019cb71d411a33d0fa8bc1cc5fd1f603df1
w9d <w9d@localhost>
2018-03-17 12:46:12 +0000

    Remove nested web worker design.
    This may need more looking at. It didn't work with Firefox earlier
    but works in Chromium. Tests somehow pass both :(

--- ed2k_hasher_basic.js
+++ ed2k_hasher_basic.js
@@ -1,59 +1,134 @@
 var ed2k_files = function(files, opts) {
 'use strict'
-  var worker = null
   var prop = { onprogress: null, onfilecomplete: null, onallcomplete: null,
     execute: execute, terminate: terminate }
 
-  function execute() {
-    var before = Date.now()
+  var reader = new FileReader()
+  var fileoffset = -1, md4_worker = new Worker('md4-worker.js'), die = false
 
-    if (!window.Worker) {
-      window.alert('Browser does not support HTML5 Web Workers.\n\nYou cannot' +
-        ' use this browser. Sorry.')
-      console.log('Browser does not support HTML5 Web Workers. You cannot' +
-        ' use this browser. Sorry.')
-      return
+  md4_worker.onerror = function(e) {
+    die = true
+    window.alert('Something wrong with HTML5 Web Worker.' +
+      ' The error is...\n\n', e.message)
+    console.log('web worker error', e)
+  }
+
+  var total_size = files.reduce(function(a,b){return a+b.size}, 0)
+  var total_multiplier = 1 / total_size
+  var total_processed = 0
+  var multipliers = files.map(function(a){return 1/a.size})
+
+  function ed2k_file(file) {
+    'use strict'
+    var offset = 0, offset_i = 0, queue = 0, chunks = [], chunks_i = [],
+        busy_read = false, busy_work = false, md4_list = []
+    die = false
+
+    md4_worker.onmessage = function(e) {
+      md4_list[e.data.index] = e.data.md4
+      busy_work = false
+      e.data.dirty = null
+      delete e.data.dirty
+      queue -= 1
+      process(true)
     }
 
-    worker = new Worker('ed2k_hasher_basic-worker.js')
-    worker.onerror = function(e) {
-      window.alert('Something wrong with HTML5 Web Worker. The error is...\n\n'
-          + e.message)
+    reader.onloadend = function(evt) {
+      chunks[offset_i] = evt.target.result
+      chunks_i.push(offset_i)
+      busy_read = false
+      offset += 9728000
+      offset_i += 1
+      if (prop.onprogress) {
+        setTimeout(prop.onprogress, 1, file,
+          multipliers[fileoffset] * offset,
+          total_multiplier * (total_processed + offset))
+      }
+      process(false)
     }
-    worker.onmessage = function(e) {
-      if (e.data.event === 1) {
-        // progress event
-        if (prop.onprogress) {
-          prop.onprogress(e.data.file, e.data.file_progress,
-            e.data.files_progress)
+
+    function process(from_md4_worker) {
+      if (die)
+        return
+      console.log('status: '+offset+'/'+file.size+' read='+busy_read+
+          ' work='+busy_work+' from_md4='+from_md4_worker+' queue='+queue)
+
+      if (queue > 0 && chunks_i.length > 0 && !busy_work) {
+        var index = chunks_i.shift()
+        var chunk = chunks[index]
+        busy_work = true
+        md4_worker.postMessage({workerid:1, index: index, data: chunk},
+            [chunk])
+        chunks[index] = null
+      }
+
+      if (offset <= file.size && !busy_read && queue < 6) {
+        busy_read = true
+        queue += 1
+        reader.readAsArrayBuffer(file.slice(offset, offset+9728000))
+      } else if (!busy_read && !busy_work && queue === 0 && from_md4_worker) {
+        if (!prop.onfilecomplete)
+          return
+
+        if (file.size >= 9728000) {
+          // calculate final hash...
+          md4_worker.onmessage = function(e) {
+            setTimeout(prop.onfilecomplete, 1, file,
+              arrayBufferToHexDigest(e.data.ed2khash))
+          }
+          md4_worker.postMessage({finish: true, md4_list: md4_list})
+          die = true
+        } else {
+          setTimeout(prop.onfilecomplete, 1, file,
+            arrayBufferToHexDigest(md4_list[0]))
         }
-      } else if (e.data.event === 2) {
-        // onfilecomplete event
-        if (prop.onfilecomplete)
-          prop.onfilecomplete(e.data.file, e.data.ed2k_hash)
-      } else if (e.data.event === 3) {
-        // onallcomplete event
-        console.log('all files processed. took ' + (Date.now() - before) + 'ms')
-        if (prop.onallcomplete)
-          prop.onallcomplete()
-      } else if (e.data.event === 100) {
-        // md4 sub worker error
-        worker.terminate()
-        worker = null
-        window.alert('Something wrong with HTML5 Sub Web Worker.' +
-          ' The error is...\n\n' + e.data.message)
-      } else {
-        window.alert('This should never ever happen.')
+        total_processed += file.size
+        processNextFile()
       }
     }
-    worker.postMessage({files: files})
+
+    process(false)
+  }
+
+  function arrayBufferToHexDigest(arr) {
+    // taken from js-md4
+    var HEX_CHARS = '0123456789abcdef'.split('')
+    var blocks = new Uint32Array(arr)
+    var h0 = blocks[0], h1 = blocks[1], h2 = blocks[2], h3 = blocks[3]
+
+    return HEX_CHARS[(h0 >> 4) & 0x0F] + HEX_CHARS[h0 & 0x0F] +
+    HEX_CHARS[(h0 >> 12) & 0x0F] + HEX_CHARS[(h0 >> 8) & 0x0F] +
+    HEX_CHARS[(h0 >> 20) & 0x0F] + HEX_CHARS[(h0 >> 16) & 0x0F] +
+    HEX_CHARS[(h0 >> 28) & 0x0F] + HEX_CHARS[(h0 >> 24) & 0x0F] +
+    HEX_CHARS[(h1 >> 4) & 0x0F] + HEX_CHARS[h1 & 0x0F] +
+    HEX_CHARS[(h1 >> 12) & 0x0F] + HEX_CHARS[(h1 >> 8) & 0x0F] +
+    HEX_CHARS[(h1 >> 20) & 0x0F] + HEX_CHARS[(h1 >> 16) & 0x0F] +
+    HEX_CHARS[(h1 >> 28) & 0x0F] + HEX_CHARS[(h1 >> 24) & 0x0F] +
+    HEX_CHARS[(h2 >> 4) & 0x0F] + HEX_CHARS[h2 & 0x0F] +
+    HEX_CHARS[(h2 >> 12) & 0x0F] + HEX_CHARS[(h2 >> 8) & 0x0F] +
+    HEX_CHARS[(h2 >> 20) & 0x0F] + HEX_CHARS[(h2 >> 16) & 0x0F] +
+    HEX_CHARS[(h2 >> 28) & 0x0F] + HEX_CHARS[(h2 >> 24) & 0x0F] +
+    HEX_CHARS[(h3 >> 4) & 0x0F] + HEX_CHARS[h3 & 0x0F] +
+    HEX_CHARS[(h3 >> 12) & 0x0F] + HEX_CHARS[(h3 >> 8) & 0x0F] +
+    HEX_CHARS[(h3 >> 20) & 0x0F] + HEX_CHARS[(h3 >> 16) & 0x0F] +
+    HEX_CHARS[(h3 >> 28) & 0x0F] + HEX_CHARS[(h3 >> 24) & 0x0F]
+  }
+
+  function processNextFile() {
+    if (files[++fileoffset]) {
+      ed2k_file(files[fileoffset])
+    } else {
+      if (prop.onallcomplete)
+        setTimeout(prop.onallcomplete, 1)
+    }
+  }
+
+  function execute() {
+    processNextFile()
   }
 
   function terminate() {
-    if (worker) {
-      worker.terminate()
-      worker = null
-    }
+    die = true
   }
 
   return prop
