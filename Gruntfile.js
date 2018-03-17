7e03aa0401a6730f5f1432f0b79d3fad1a8a19a3
w9d <w9d@localhost>
2018-03-17 17:05:11 +0000

    Without nested web workers, electron can do headless testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ grunt.initConfig({
     target: ['Gruntfile.js', 'test/*.js', '*.js']
   },
   shell: {
-    taperunfirefox: {
-      command: '/usr/bin/firefox "http://localhost:8081" & ./node_modules/browserify/bin/cmd.js ed2khash_test.js|./node_modules/tape-run/bin/run.js --wait 60 --static . --port 8081'
+    taperun: {
+      command: './node_modules/browserify/bin/cmd.js ./test/ed2khash_test.js|./node_modules/tape-run/bin/run.js --wait 60 --static src'
     },
     zuullocal: {
       command: 'zuul --ui tape --local 9000 ../test/ed2khash_test.js',
@@ -27,6 +27,7 @@ grunt.loadNpmTasks('grunt-shell')
 grunt.registerTask('hint', ['jshint'])
 grunt.registerTask('standard', ['eslint'])
 grunt.registerTask('test-local', ['shell:zuullocal'])
+grunt.registerTask('test-tape', ['shell:taperun'])
 grunt.registerTask('default', [])
 }
 
