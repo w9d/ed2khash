ab0f795f0f69778a5b26de99e55ec64744ba19ea
w9d <w9d@localhost>
2018-03-19 19:03:17 +0000

    test: reformat for lint tool

--- test/common.js
+++ test/common.js
@@ -2,30 +2,31 @@
 
 const xorshift7 = require('./xorshift7.js')
 
-var genZero = function(size) {
+var genZero = function (size) {
   var buffer = new Uint8Array(size)
   return buffer.buffer
 }
 
-var genRand = function(size, seed) {
+var genRand = function (size, seed) {
   var buffer = new Int8Array(size)
-  var xors7 = xorshift7(seed || 1963272565), data
-
-  for(var start = 0; start < size; start += 4) {
+  var xors7 = xorshift7(seed || 1963272565)
+  var data
+  for (var start = 0; start < size; start += 4) {
     data = xors7.int32()
-    //console.log('loop outer start=' + start + ' size=' + size)
+    // console.log('loop outer start=' + start + ' size=' + size)
     for (var off = start, i = 0;
-        off < start + 4 && start + i < size;
-        off += 1, i += 1) {
-      buffer[off] = (data >> i*8) & 0xff
-      //console.log('loop inner start=' + start, 'off=' + off, 'shift=' + i*8)
+      off < start + 4 && start + i < size;
+      off += 1, i += 1) {
+      buffer[off] = (data >> i * 8) & 0xff
+      // console.log('loop inner start=' + start, 'off=' + off, 'shift=' + i*8)
     }
   }
   return buffer.buffer
 }
 
-var genFile = function(generator, size, options) {
-  var opt = options || {}, file
+var GenFile = function (generator, size, options) {
+  var opt = options || {}
+  var file
   try {
     file = new File([generator(size, opt.seed)], opt.name || 'test.mp4',
       {type: 'video/mp4'})
@@ -39,5 +40,5 @@ var genFile = function(generator, size, options) {
 module.exports = {
   genZero: genZero,
   genRand: genRand,
-  genFile: genFile
+  GenFile: GenFile
 }
